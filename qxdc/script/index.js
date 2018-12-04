b9f762f1e66043525c4425decee9ab674d022d5d
my123jian <my123jian@gmail.com>
2018-12-04 21:20:06 +0800

    添加迁徙洞察

---
+++ qxdc/script/index.js
@@ -0,0 +1,117 @@
+$(function () {
+    var theIntervals = 1000 * 60 * 5;
+    //视图类型
+    var ViewType = {
+        OUT: "OUT",//迁出洞察
+        IN: "IN",//迁入洞察
+        PROVINCE: "PROVINCE"//省内迁徙
+    };
+    //迁徙方向
+    var DirectionType = {
+        SHENG: "SHENG",//省
+        GAT: "GAT",//港澳台
+        JW: "JW"//境外
+    }
+    var theCurrentView = "";
+    var theDirection = "";
+    var isStopRefresh = true;
+    var theTimer = null;
+
+    function PageViewModel() {
+        this.initEvent();
+        this.start();
+    }
+
+    /**
+     * 初始化页面事件
+     */
+    PageViewModel.prototype.initEvent = function () {
+
+    }
+    /**
+     * 开始自动查询数据
+     */
+    PageViewModel.prototype.start = function () {
+        if (isStopRefresh) {
+            isStopRefresh = false;
+        }
+        else {
+            return;
+        }
+        var me = this;
+        theTimer = window.setInterval(function () {
+            me.refresh();
+        }, theIntervals);
+    }
+    /**
+     * 停止查询数据
+     */
+    PageViewModel.prototype.stop = function () {
+        if (theTimer) {
+            window.clearInterval(theTimer);
+            theTimer = null;
+        }
+    }
+    /**
+     * 刷新当前的数据
+     */
+    PageViewModel.prototype.refresh = function () {
+
+    }
+    /***
+     * 切换方向
+     */
+    PageViewModel.prototype.switchDirection = function () {
+
+    }
+    /**
+     * 迁出视图
+     */
+    PageViewModel.prototype.initInView = function () {
+
+    }
+    /**
+     * 迁入视图
+     */
+    PageViewModel.prototype.initOutView = function () {
+
+    }
+    /**
+     * 省内视图
+     */
+    PageViewModel.prototype.initProvinceView = function () {
+
+    }
+    /***
+     * 切换视图
+     * @param viewName
+     */
+    PageViewModel.prototype.switchView = function (viewName) {
+        if (theCurrentView == viewName) {
+            console.log("视图类型未改变" + viewName);
+            return;
+        }
+        switch (viewName) {
+            case ViewType.IN:
+                theDirection = DirectionType.SHENG;//设置为港澳台
+                theCurrentView = viewName;
+                this.initInView();
+                break;
+            case  ViewType.OUT:
+                theDirection = DirectionType.SHENG;//设置为港澳台
+                theCurrentView = viewName;
+                this.initOutView();
+                break;
+            case ViewType.PROVINCE:
+                theDirection = DirectionType.SHENG;//设置为港澳台
+                theCurrentView = viewName;
+                this.initProvinceView();
+                break;
+            default:
+                console.log("视图类型错误:" + viewName);
+                return;
+        }
+
+    }
+    window.PageView = new PageViewModel();
+})
--- qxyc/script/index.js
+++ qxyc/script/index.js
@@ -93,6 +93,22 @@ $(function () {
                 show: true
             });
         });
+        var me=this;
+        laydate.render({
+            elem: '#date-input', //指定元素
+            trigger: 'click',
+            value:new Date(),
+            done:function(value, date, endDate){
+                //debugger;
+                console.log('日期变化:'+value); //得到日期生成的值，如：2017-08-18
+                console.log(date); //得到日期时间对象：{year: 2017, month: 8, date: 18, hours: 0, minutes: 0, seconds: 0}
+                console.log(endDate); //得结束的日期时间对象，开启范围选择（range: true）才会返回。对象成员同上。
+            }
+        });
+        /*$('#date-input').change(function(){
+            theCurrentDate=$(this).val();
+            console.log('日期变化:'+theCurrentDate);
+        });*/
     }
     PageViewModel.prototype.initChartMap = function () {
         echarts.registerMap('gd', theGdData);
