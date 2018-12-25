5446acc9f86f9557131d511f6f68066ef725552d
my123jian <my123jian@gmail.com>
2018-12-25 17:27:14 +0800

    加入区域绘制代码 调整编辑 绘制热力图

--- yjzx/js/map.js
+++ yjzx/js/map.js
@@ -35,7 +35,6 @@ $(function () {
     var theMap2 = null;
 
 
-
     function floorBindClick() {
         var floors = $('#DivButton div');
         for (var i = 0; i < floors.length; i++) {
@@ -119,77 +118,77 @@ $(function () {
                     }
 
                 }
-                console.log('theBigBounds',theBigBounds);
+                // console.log('theBigBounds',theBigBounds);
 
                 me.showLine(theBigBounds, theMap, theMap2);
 
             })
         });
         this.initEvent();
-      //  标注城市
-      var theCitys = {
-        "广州市": "113.264385,23.129112",
-        "深圳市": "114.085947,22.547",
-        "珠海市": "113.553986,22.224979",
-        "汕头市": "116.708463,23.37102",
-        "佛山市": "113.122717,23.028762",
-        "韶关市": "113.597313,24.811094",
-        "河源市": "114.697802,23.746266",
-        "梅州市": "116.117582,24.299112",
-        "惠州市": "114.412599,23.079404",
-        "汕尾市": "115.364238,22.774485",
-        "东莞市": "113.746262,23.046237",
-        "中山市": "113.382391,22.521113",
-        "江门市": "113.094942,22.590431",
-        "阳江市": "111.982697,21.857415",
-        "湛江市": "110.364977,21.274898",
-        "茂名市": "110.919229,21.659751",
-        "肇庆市": "112.472529,23.051546",
-        "清远市": "113.051227,23.685022",
-        "潮州市": "116.632301,23.661701",
-        "揭阳市": "116.355733,23.543778",
-        "云浮市": "112.044439,22.929801"
-      };
-      for (var theCityName in theCitys) {
-        var thelngLat = theCitys[theCityName];
-        var thelngLats = thelngLat.split(',');
-        var marker = new AMap.Marker({
-          position: new AMap.LngLat(parseFloat(thelngLats[0]), parseFloat(thelngLats[1])),   // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
-          title: theCityName,
-          content: '<div style="color:white;width: 3em;font-size:20px;font-weight:bold">' + theCityName + '</div>'
-        });
-        theMap.add(marker);
-      }
+        //  标注城市
+        var theCitys = {
+            "广州市": "113.264385,23.129112",
+            "深圳市": "114.085947,22.547",
+            "珠海市": "113.553986,22.224979",
+            "汕头市": "116.708463,23.37102",
+            "佛山市": "113.122717,23.028762",
+            "韶关市": "113.597313,24.811094",
+            "河源市": "114.697802,23.746266",
+            "梅州市": "116.117582,24.299112",
+            "惠州市": "114.412599,23.079404",
+            "汕尾市": "115.364238,22.774485",
+            "东莞市": "113.746262,23.046237",
+            "中山市": "113.382391,22.521113",
+            "江门市": "113.094942,22.590431",
+            "阳江市": "111.982697,21.857415",
+            "湛江市": "110.364977,21.274898",
+            "茂名市": "110.919229,21.659751",
+            "肇庆市": "112.472529,23.051546",
+            "清远市": "113.051227,23.685022",
+            "潮州市": "116.632301,23.661701",
+            "揭阳市": "116.355733,23.543778",
+            "云浮市": "112.044439,22.929801"
+        };
+        for (var theCityName in theCitys) {
+            var thelngLat = theCitys[theCityName];
+            var thelngLats = thelngLat.split(',');
+            var marker = new AMap.Marker({
+                position: new AMap.LngLat(parseFloat(thelngLats[0]), parseFloat(thelngLats[1])),   // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
+                title: theCityName,
+                content: '<div style="color:white;width: 3em;font-size:20px;font-weight:bold">' + theCityName + '</div>'
+            });
+            theMap.add(marker);
+        }
 
 
-      var thePoints = {
+        var thePoints = {
 //    "广州南站": {"latitude": 22.9874720000, "longitude": 113.2685860000, "type": "铁路", 'icon': 'tielu3.png'},
-        // "深圳火车站": { "latitude": 22.5319900000, "longitude": 114.1176800000 },
+            // "深圳火车站": { "latitude": 22.5319900000, "longitude": 114.1176800000 },
 //    "深圳北站": {"latitude": 22.6097250000, "longitude": 114.0291130000, "type": "铁路", 'icon': 'tielu3.png'},
-        //"广州东站": { "latitude": 23.1505660000, "longitude": 113.3249000000 },
-        //"广州火车站": { "latitude": 23.1494150000, "longitude": 113.2572910000 },
-        //"广州北站": { "latitude": 23.3774050000, "longitude": 113.2037940000 },
-        //"深圳西站": { "latitude": 22.5275730000, "longitude": 113.9073060000 },
-        //"深圳东站": { "latitude": 22.6019860000, "longitude": 114.1199340000 },
+            //"广州东站": { "latitude": 23.1505660000, "longitude": 113.3249000000 },
+            //"广州火车站": { "latitude": 23.1494150000, "longitude": 113.2572910000 },
+            //"广州北站": { "latitude": 23.3774050000, "longitude": 113.2037940000 },
+            //"深圳西站": { "latitude": 22.5275730000, "longitude": 113.9073060000 },
+            //"深圳东站": { "latitude": 22.6019860000, "longitude": 114.1199340000 },
 //    "珠海站": {"latitude": 22.2153960000, "longitude": 113.5496410000, "type": "铁路", 'icon': 'tielu3.png'},
-        //"白云国际机场": { "latitude": 23.3896270000, "longitude": 113.3076480000 },
+            //"白云国际机场": { "latitude": 23.3896270000, "longitude": 113.3076480000 },
 //    "白云国际机场": {"latitude": 23.396544, "longitude": 113.306199, "type": "民航", 'maxLevel': 17, 'icon': 'feiji3.png'},
-        //"宝安国际机场": { "latitude": 22.6333600000, "longitude": 113.8145490000 },
+            //"宝安国际机场": { "latitude": 22.6333600000, "longitude": 113.8145490000 },
 //    "宝安国际机场": {"latitude": 22.62506, "longitude": 113.812809, "type": "民航", 'icon': 'feiji3.png'},
-        //"珠海金湾国际机场": { "latitude": 22.0057560000, "longitude": 113.3819450000 },
-        //"揭阳潮汕国际机场": { "latitude": 23.5463610000, "longitude": 116.5092740000 }
-      };
+            //"珠海金湾国际机场": { "latitude": 22.0057560000, "longitude": 113.3819450000 },
+            //"揭阳潮汕国际机场": { "latitude": 23.5463610000, "longitude": 116.5092740000 }
+        };
 
-      for (var theCityName in thePoints) {
-        var thelngLat = thePoints[theCityName];
+        for (var theCityName in thePoints) {
+            var thelngLat = thePoints[theCityName];
 //    debugger;
-        var marker = new AMap.Marker({
-          position: new AMap.LngLat(thelngLat.longitude, thelngLat.latitude),   // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
-          title: theCityName,
-          content: '<div style="color:white;width: 3em;font-size:14px">' + theCityName + '</div>'
-        });
-        theMap.add(marker);
-      }
+            var marker = new AMap.Marker({
+                position: new AMap.LngLat(thelngLat.longitude, thelngLat.latitude),   // 经纬度对象，也可以是经纬度构成的一维数组[116.39, 39.9]
+                title: theCityName,
+                content: '<div style="color:white;width: 3em;font-size:14px">' + theCityName + '</div>'
+            });
+            theMap.add(marker);
+        }
 
     }
 
@@ -252,16 +251,17 @@ $(function () {
             if (theZoom >= 12) {
                 // console.log("显示点");
                 // console.log()
-              debugger
-              if(me.isGaoSuLuWang) {
-                theMap.setFeatures(['bg','road']);
-              }
-              else {
-                  // debugger
-                theMap.setFeatures(['bg', 'building', 'point']);
-                theMap.add(roadNet);
-                theMap.add(building);
-              }
+                // debugger
+                if (me.isGaoSuLuWang) {
+                    theMap.setFeatures(['bg', 'road']);
+                }
+                else {
+                    // debugger
+                    theMap.setFeatures(['bg', 'building', 'point', 'road']);
+                    // theMap.add(roadNet);
+                    // theMap.add(traffic);
+                    theMap.add(building);
+                }
 
                 theMap.setPitch(45);
                 theInnerLayer && theInnerLayer.setzIndex(1000);
@@ -271,21 +271,21 @@ $(function () {
             }
             else {
                 console.log("隐藏点");
-                if(me.isGaoSuLuWang) {
-                  theMap.setFeatures(['bg','road']);
+                if (me.isGaoSuLuWang) {
+                    theMap.setFeatures(['bg', 'road']);
 
-                }else {
+                } else {
                     theMap.setFeatures(['bg']);
 
                 }
 
-              //theHeartLayer && theHeartLayer.setMap(null);
+                //theHeartLayer && theHeartLayer.setMap(null);
                 theHeartLayer && theHeartLayer.remove();
                 theHeartLayer = null;
                 // theMap.setFeatures(['bg', 'building']);
                 theMap.remove(roadNet);
                 theMap.remove(building);
-                //theMap.remove(satellite);
+                // theMap.remove(traffic);
                 theMap.setPitch(0);
                 //theMap.setMapStyle("amap://styles/grey");
             }
@@ -331,6 +331,7 @@ $(function () {
                 }
                 var theLastBuilding = theBuilding;
                 theBuilding = theInnerLayer.getSelectedBuilding();
+                // console.log(theBuilding)
                 if (theBuilding != theLastBuilding) {
 
                     if (!theBuilding) {
@@ -343,6 +344,7 @@ $(function () {
                     //floor_complete
                     var theFloors = theBuilding.floor_details.floor_nonas;
                     var theFloorIndex = theBuilding.floor_details.floor_indexs;
+
                     for (var i = 0; i < theFloors.length; i++) {
                         var theName = theFloors[i];
                         var theIndex = theFloorIndex[i];
@@ -351,14 +353,14 @@ $(function () {
                             theInnerLayer.showFloor(theCurrentIndex);
                             var theCurrentBuild = theInnerLayer.getSelectedBuilding();
                             var theLnt = theCurrentBuild.lnglat;
-                            me.drawReli(theLnt.lng, theLnt.lat);
+                            me.drawReli(theLnt.lng, theLnt.lat);  // 热力图
 
                         }).appendTo($('#DivButton'));
                         console.log(theFloors[i]);
                         //开始显示楼层
                         //theBuilding.showFloor();
                     }
-                    ;
+
                     floorBindClick();
                 }
             }, 500);
@@ -413,13 +415,12 @@ $(function () {
      * 设置地图为默认样式
      */
     MapBase.prototype.restoreDefaultStyle = function () {
+        this.isGaoSuLuWang = false;
+        this.hideOtherProvince(false);
         theMap.setMapStyle(theDefaultMapStyle);
         theMap2.setMapStyle(theDefaultMapStyle);
-        this.hideOtherProvince(false);
         theMap.remove(traffic);
         theMap.remove(roadNet);
-      this.isGaoSuLuWang = false;
-
     };
     /**
      * 设置为 高速路网样式
@@ -427,15 +428,15 @@ $(function () {
     MapBase.prototype.setTrafficStyle = function () {
         // theMap.setMapStyle('');
         // theMap2.setMapStyle('');
-      this.isGaoSuLuWang = true;
-      this.hideOtherProvince(false);
+        this.isGaoSuLuWang = true;
+        this.hideOtherProvince(true);
 
         theMap.remove(traffic);
         theMap.remove(roadNet);
 
         theMap.add(traffic);
         // theMap.add(roadNet);
-      theMap.setMapStyle(theMapStyle2);
+        theMap.setMapStyle(theMapStyle2);
 
     }
 
@@ -602,38 +603,38 @@ $(function () {
         }, 10);
     }
 
-  MapBase.prototype.hideOtherProvince = function (status) {
+    MapBase.prototype.hideOtherProvince = function (status) {
 
-    if (!this.provinceLayer) {
-      var thePaths=theGdProvincePath.map(function (m) {
-        return [m.lng,m.lat];
-      });
-      var thePath2=[
-        [-180, -90],
-        [-180, 90],
-        [180, 90],
-        [180, -90],
-        [-180, -90]
-      ];
-      // debugger;
-      this.provinceLayer = new AMap.Polygon({
-        map: theMap,
-        //strokeWeight: 10,
-        fillColor:'#132c58',
-        fillOpacity: 1,
-        //strokeColor:'green',
+        if (!this.provinceLayer) {
+            var thePaths = theGdProvincePath.map(function (m) {
+                return [m.lng, m.lat];
+            });
+            var thePath2 = [
+                [-180, -90],
+                [-180, 90],
+                [180, 90],
+                [180, -90],
+                [-180, -90]
+            ];
+            // debugger;
+            this.provinceLayer = new AMap.Polygon({
+                map: theMap,
+                //strokeWeight: 10,
+                fillColor: '#132c58',
+                fillOpacity: 1,
+                //strokeColor:'green',
 
-        path:[thePath2,thePaths]
-      });
+                path: [thePath2, thePaths]
+            });
+        }
+        if (!status) {
+            this.provinceLayer.hide();
+        }
+        else {
+            this.provinceLayer.show();
+            //theMap.setFitView(this.provinceLayer);
+        }
     }
-    if (!status) {
-      this.provinceLayer.hide();
-    }
-    else {
-      this.provinceLayer.show();
-      //theMap.setFitView(this.provinceLayer);
-    }
-  }
 
     MapBase.prototype.SwitchView = function (viewName) {
         this.ReturnDefualt();
@@ -681,38 +682,89 @@ $(function () {
                 container: map,
                 type: 'heatmap',
                 // 基本热力图
-                shape: 'normal'
+                shape: 'normal',
+                zIndex: 10001
             });
+            theHeartLayer.setZIndex(10001);
         }
     }
 
 
-    MapBase.prototype.drawReli = function (lng, lat, data) {
+    MapBase.prototype.drawReli = function (name, data) {
+        var theAreaPath = {
+            '深圳北':
+                [[114.0286, 22.608196], [114.027709, 22.609159], [114.029178, 22.610417], [114.02955, 22.610698], [114.030455, 22.609765]],
+            '广州南':
+                [[113.26794, 22.987108], [113.26713, 22.988296], [113.270557, 22.990331], [113.271209, 22.989086], [113.269443, 22.987991]],
+            '白云机场':
+                [[113.301301, 23.385968], [113.301723, 23.387568], [113.304613, 23.386954], [113.304136, 23.385315]],
+            '宝安机场':
+                [[113.811961, 22.623893], [113.811513, 22.624723], [113.812374, 22.625131], [113.813637, 22.625844], [113.814257, 22.626182], [113.814571, 22.62592], [113.814919, 22.625322]],
+            '广州站':
+                [[113.256631, 23.148886], [113.256898, 23.149427], [113.25737, 23.149234], [113.259263, 23.148291], [113.258991, 23.147797]],
+
+            '广州站广场':
+                [[113.257712, 23.148006], [113.257435, 23.147872], [113.25721, 23.147558], [113.257028, 23.147511], [113.25695, 23.147561], [113.256622, 23.14829], [113.256618, 23.14858], [113.256697, 23.148751], [113.257154, 23.148542], [113.258192, 23.148094], [113.258907, 23.147809], [113.258765, 23.147579]],
+
+
+        };
         this.CreateHeartLayer();
-        lng = lng || 113.23;
-        lat = lat || 23.16;
-        var theValue = Math.floor((Math.random() * 10));
-        layer = theHeartLayer;
-        var list = [];
-        var i = -1, length = 400;
-        while (++i < length) {
-            //var item = heatmapData[i + theValue];
-            //i = i + theValue;
-
-            list.push({
-                coordinate: [lng + Math.random() / 1000, lat + Math.random() / 1000],
-                count: Math.floor((Math.random() * 100))
-            })
+        value = data || 1000;
+        var path = theAreaPath[name];
+        if (!path) {
+            console.log("未找到对应的范围");
+            return;
         }
+        var thePorints = path;
+        var lngs = thePorints.map(function (item) {
+            return item[0];
+        });
+        var lats = thePorints.map(function (item) {
+            return item[1];
+        });
 
-        layer.setData(list, {
+        var minLngs = lngs.min();
+        var maxLngs = lngs.max();
+        var minLats = lats.min();
+        var maxLats = lats.max();
+
+
+        var theValidPoints = [];
+        for (var i = 0; i < 1000; i++) {
+            var thePoint = Math.RandomPoint(minLngs, maxLngs, minLats, maxLats);
+            if (AMap.GeometryUtil.isPointInRing(thePoint, thePorints)) {
+                theValidPoints.push(thePoint);
+            }
+        }
+        var layer = theHeartLayer;
+
+        var theCurrentValue = value;
+        var theShowList = [];
+        var list = theValidPoints.map(function (item) {
+            var theTempValue = 0;
+            if (theCurrentValue <= 10) {
+                theTempValue = theCurrentValue;
+            }
+            else {
+                theTempValue = Math.floor(Math.RandomRange(1, 10));
+            }
+            theCurrentValue -= theTempValue;
+            if (theTempValue > 0) {
+                theShowList.push({
+                    coordinate: [item[0], item[1]],
+                    count: Math.floor(theTempValue)
+                });
+            }
+
+        })
+        layer.setData(theShowList, {
             lnglat: 'coordinate',
             value: 'count'
         });
 
         layer.setOptions({
             style: {
-                radius: 30,
+                radius: 10,
                 color: {
                     0.5: '#2c7bb6',
                     0.65: '#abd9e9',
@@ -725,7 +777,6 @@ $(function () {
 
         layer.render();
     }
-
 
 
     window.MapBase = MapBase;
