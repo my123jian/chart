9f339db9692d2940a928a6f8d2340b3303070489
jian <my123jian@gmal.com>
2019-06-30 22:10:04 +0800

    解决高德地图和echart点结合,更新组件信息

--- chart-mobile/src/components/EchartAMap.vue
+++ chart-mobile/src/components/EchartAMap.vue
@@ -6,8 +6,9 @@
 <script>
     import CityMap from '../utils/CityMap';
     import ProvinceMap from '../utils/ProvinceMap';
-    import axios from "axios";
+    import CityCodeMap from '../utils/CityCodeMap';
     import GeoUtil from '../utils/GeoUtil';
+    import GpsUtil from '../utils/GpsUtil';
 
     export default {
         name: "EchartAMap",
@@ -34,6 +35,18 @@
                 var me = this;
                 this.chartMap = window.echarts.init(this.$refs.chartMap);
                 this.chartMap.setOption({
+                    backgroundColor: '',
+                    title: {
+                        text: '',
+                        //subtext: '数据覆盖率',
+                        left: 'center',
+                        textStyle: {
+                            color: '#fff'
+                        }
+                    },
+                    tooltip: {
+                        trigger: 'item'
+                    },
                     amap: {
                         maxPitch: 60,
                         pitch: 10, //45 俯仰角
@@ -56,6 +69,219 @@
                         series: theOptions.series
                     });
                 }
+            },
+            /**城市编码和名称的对照*/
+            getCityNameByCode(name) {
+                return name;
+            },
+            /*根据名字得到经纬度**/
+            geoCoordMap(name) {
+
+                if (this.areaMod == 1) {
+                    var theCityCode = CityCodeMap.getCountyCode("广东省", this.cityName, name);
+                    debugger;
+                    return GpsUtil.getByAreaCode(theCityCode);
+                }
+                else {
+                    return GpsUtil.getByCityName(name);
+                }
+
+            },
+            /**根据名称转换经纬度*/
+            convertData(data) {
+                var res = [];
+                if (!data) {
+                    return {
+                        lines: [],
+                        points: []
+                    };
+                }
+                var theValidPoints = [];
+                for (var i = 0; i < data.length; i++) {
+                    var dataItem = data[i];
+
+                    var fromCoord = this.geoCoordMap(dataItem.from);
+                    var toCoord = this.geoCoordMap(dataItem.to);
+                    if (fromCoord && toCoord) {
+                        var theLine = {
+                            fromName: dataItem.from,
+                            toName: dataItem.to,
+                            coords: [fromCoord, toCoord],
+                            //设置线段颜色
+                            lineStyle: {
+                                normal: {
+                                    color: this.colors[i > 10 ? 9 : i],
+                                    width: 2,
+                                    curveness: 0.2
+                                }
+                            }
+                        };
+                        theValidPoints.push(dataItem);
+                        res.push(theLine);
+                    }
+                }
+                return {
+                    lines: res,
+                    points: theValidPoints
+                };
+            },
+            /**根据数据进行呈现*/
+            refresh(data) {
+                var theMapName = data.name;
+                var theMapPath = this.getMapPath(theMapName);
+                var me = this;
+                if (this.mapName != theMapName) {
+
+                }
+                me.drawMap(data);
+
+            },
+            /**根据数据呈现到地图上*/
+            drawMap(data) {
+                // name
+                // from to value
+                var me = this;
+                var theMapName = data.name;
+                var theItems = data.items || [];
+                var theMapHash = {};
+
+                var planePath = 'path://M1705.06,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705.06,1318.313z';
+
+                var theConvertResult = this.convertData(data.items);
+
+                var thePoints = theConvertResult.points;
+                var theLines = theConvertResult.lines;
+
+                //var color = ['#a6c84c', '#ffa022', '#46bee9'];
+                var color = ['#49ffff'];//
+                var series = [];
+
+                series.push(
+                    {
+                        // name: item[0] + ' Top10',
+                        type: 'lines',  //静态线
+                        zlevel: 1,
+                        effect: {
+                            show: false,
+                            period: 6,
+                            trailLength: 0.7,
+                            color: '#49ffff',//'#fff',
+                            symbolSize: 3
+                        },
+                        lineStyle: {
+                            normal: {
+                                color: '#49ffff',//color[i],
+                                width: 1,
+                                curveness: 0.2
+                            }
+                        },
+                        data: theLines,
+                    },
+                    {
+                        //name: item[0] + ' Top10',
+                        type: 'lines',  //动态线
+                        zlevel: 2,
+                        effect: {
+                            show: true,
+                            period: 6,
+                            trailLength: 0.7,
+                            color: '#fff',
+                            symbol: 'arrow',
+                            symbolSize: 5,
+                            // shadowBlur: 10,
+                        },
+                        lineStyle: {
+                            normal: {
+                                color: color[0],
+                                width: 1,
+                                opacity: 0.5,
+                                curveness: 0.2,
+                                type: 'solid',
+                                // shadowBlur: 5,
+                                // shadowColor: color[i],
+                            }
+                        },
+                        // emphasis: {
+                        //   lineStyle: {
+                        //     color: {
+                        //       type: 'linear',
+                        //       x: 0,
+                        //       y: 0,
+                        //       x2: 0,
+                        //       y2: 1,
+                        //       colorStops: [{
+                        //           offset: 0, color: 'red' // 0% 处的颜色
+                        //       }, {
+                        //           offset: 1, color: 'blue' // 100% 处的颜色
+                        //       }],
+                        //       globalCoord: false // 缺省为 false
+                        //     }
+                        //   }
+                        // },
+                        symbol: ['none', 'arrow'],
+                        symbolSize: 10,
+                        data: theLines,
+
+                    },
+                    {
+                        //name: item[0] + ' Top10',
+                        type: 'effectScatter',
+                        // symbol:'emptyCircle',
+                        // markPoint: {
+                        //   symbol: 'circle',
+                        //   data: [
+                        //     {
+                        //       name: '某个坐标',
+                        //       coord: [110.365554,21.276724],
+                        //       value:965,
+
+                        //     }
+                        //   ]
+                        // },
+                        //   animationDelayUpdate: function (idx) {
+                        //     return 1000;
+                        // },
+                        effectType: 'ripple',
+
+                        hoverAnimation: true,
+                        coordinateSystem: 'geo',
+                        zlevel: 2,
+                        rippleEffect: {
+                            brushType: 'stroke'
+                        },
+                        label: {
+                            normal: {
+                                show: true,
+                                position: 'right',
+                                formatter: '{b}'
+                            }
+                        },
+                        symbolSize: function (val) {
+                            return 8 + (val[2] / 10000);
+                        },
+                        itemStyle: {
+                            normal: {
+                                color: color[0],
+                                shadowBlur: 10,
+                                shadowColor: '#333'
+                            }
+                        },
+                        data: thePoints.map(function (dataItem) {
+                            return {
+                                name: me.queryDirection == 1 ? dataItem.from : dataItem.to,
+                                value: me.geoCoordMap(me.queryDirection == 1 ? dataItem.from : dataItem.to).concat([dataItem.value])
+                            };
+                        }),
+                    }
+                );
+                var option = {
+                    series: series,
+
+                };
+
+                this.chartMap.clear();
+                this.chartMap.setOption(option, true);
+
             }
         },
         watch: {
